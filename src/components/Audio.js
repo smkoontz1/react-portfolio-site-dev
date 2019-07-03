08ce174a565ae1af5d9efc39d2d7fac94103b333
Steven Koontz <smkoontz1@gmail.com>
2019-07-02 23:04:26 -0500

    Audio page done

--- src/components/Audio.js
+++ src/components/Audio.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
-import Placeholder from './Placeholder';
 
 class Audio extends Component {
 
   render() {
     return(
-      <Placeholder pageName="AUDIO" />
+      <div>
+        <section className="section">
+          <div className="container">
+            <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/588166755&color=%23311e14&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
+            <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/584402157&color=%23311e14&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
+            <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/584401689&color=%23311e14&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
+          </div>
+        </section>
+        <section className="section">
+          <div className="container">
+            <article className="message is-dark">
+              <div class="message-body">
+                NOTE: SoundCloud is currently not allowing registration to use their API, so the tracks on this page were embedded
+                manually. As such, this page may not include everything on my SoundCloud profile.
+              </div>
+            </article>
+          </div>
+        </section>
+      </div>
     );
   }
 
